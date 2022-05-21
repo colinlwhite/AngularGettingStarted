6e0b67640a9aa4bbf1c88982f4ade1fcf6ccdc05
Colin White <Colin.White@alogent.com>
2022-05-21 11:17:45 -0400

    filtering the products by the 'filter by' input text

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -21,8 +21,10 @@ export class ProductListComponent implements OnInit {
     set listFilter(value: string) {
         this._listFilter = value;
         console.log('In setter:', value);
+        this.filteredProducts = this.performFilter(value);
     }
 
+    filteredProducts: IProduct[] = []; 
     products: IProduct[] = [
         {
             "productId": 2,
@@ -52,5 +54,11 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().includes(filterBy));
     }
 }
