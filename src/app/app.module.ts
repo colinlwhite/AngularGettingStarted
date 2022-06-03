c65fc789db1f6f6da6ddd8df46d5d0a4606d21f5
Colin White <Colin.White@alogent.com>
2022-06-02 21:30:42 -0400

    generated module via ng CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule, CanActivate } from '@angular/router';
 import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
+    // Only Components, Directives and Pipes
     AppComponent,
     ProductListComponent,
     ConvertToSpaces,
@@ -33,8 +35,12 @@ import { ProductDetailGuard } from './products/product-detail.guard';
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
+  // Root Application Component - AppComponent
+  // Bootstrap Array should only be used in Root App Module - AppModule
+  // Other Angular Modules won't have bootstrap Array
   bootstrap: [AppComponent]
 })
 export class AppModule { }
