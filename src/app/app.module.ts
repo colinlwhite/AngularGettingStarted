50336be6b98f02e24ff761af81bcc4433b4cf580
Colin White <Colin.White@alogent.com>
2022-06-05 18:32:23 -0400

    application working with feature module addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule, CanActivate } from '@angular/router';
 import { ProductModule } from './products/product.module';
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,28 +1,33 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpaces } from '../shared/convert-to-spaces.pipe';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
-import { RouterModule, CanActivate } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { StarComponent } from '../shared/star.component';
+// import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
+    ProductDetailComponent,
     ConvertToSpaces,
-    StarComponent,
-    ProductDetailComponent
+    StarComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id',
-      canActivate: [ProductDetailGuard],
-      component: ProductDetailComponent },
-    ])
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent
+      }
+    ]),
+    ///SharedModule
   ]
 })
 export class ProductModule { }
